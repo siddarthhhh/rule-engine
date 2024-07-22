553ab497c9b0ec89dc3e096f636d5b3859042c31
siddarthhhh <chidurulasiddarth@gmail.com>
2024-07-23 01:10:02 +0530

    adding basic server,model

---
+++ backend/models/rule.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const ruleSchema = new Schema({
+  name: { type: String, required: true },
+  ruleString: { type: String, required: true },
+}, {
+  timestamps: true,
+});
+
+const Rule = mongoose.model('Rule', ruleSchema);
+
+module.exports = Rule;
